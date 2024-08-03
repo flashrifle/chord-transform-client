9e43393735e91fa705359ae31424a84a5b4cf7f2
flashrifle <flashrifle@gmail.com>
2024-08-03 17:41:41 +0900

    TEST: S3 업로드 테스트 코드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 import Image from 'next/image';
-import styles from './page.module.css';
 import axios from 'axios';
-import { Simulate } from 'react-dom/test-utils';
-import reset = Simulate.reset;
+import AWS from 'aws-sdk';
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_AWS_ACCESS_KEY);
+  console.log(process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY);
   const click = () => {
     console.log('!!!');
   };
@@ -21,6 +21,45 @@ export default function Home() {
       <h1>악보 변환 사이트</h1>
       <button onClick={getTest}>GET</button>
       <button onClick={postTest}>POST</button>
+      <input type="file" onChange={onFileUpload} />
     </>
   );
+}
+
+function onFileUpload(e) {
+  const ACCESS_KEY = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY;
+  const SECRET_ACCESS_KEY = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
+  const REGION = process.env.NEXT_PUBLIC_AWS_REGION;
+  const S3_BUCKET = process.env.NEXT_PUBLIC_AWS_S3_BUCKET;
+
+  // AWS ACCESS KEY를 세팅합니다.
+  AWS.config.update({
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  });
+
+  // 버킷에 맞는 이름과 리전을 설정합니다.
+  const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET },
+    region: REGION,
+  });
+
+  const file = e.target.files[0];
+
+  // 파일과 파일이름을 넘겨주면 됩니다.
+  const params = {
+    ACL: 'public-read',
+    Body: file,
+    Bucket: S3_BUCKET,
+    Key: file.name,
+  };
+
+  myBucket
+    .putObject(params)
+    .on('httpUploadProgress', (evt) => {
+      alert('SUCCESS');
+    })
+    .send((err) => {
+      if (err) console.log(err);
+    });
 }
